72b31a17a5756f55b2bd5f78c24bd35e9e28a159
pondsuyeon <pondsuyeon@gmail.com>
2018-05-02 22:22:52 +0900

    hioo

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var bodyPaser = require('body-parser')
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
