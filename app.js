567e57c5992791031faf343ab76691818865583e
pondsuyeon <pondsuyeon@gmail.com>
2018-05-11 18:39:43 +0900

    login yeeeeeeet

--- app.js
+++ app.js
@@ -1,54 +1,76 @@
-
 /**
  * Module dependencies.
  */
 
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
-var bodyPaser = require('body-parser')
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , bodyPaser = require('body-parser')
+var mysql= require('mysql');
 var app = express();
-
+var client = mysql.createConnection({
+   hostname:"127.0.0.1:3306", 
+   user : "root",
+   password:"1234",
+   database:"darack"
+});
+client.connect(function(err){
+   console.log('MysqlConnection');
+   if(err){console.error(err);
+   throw err;
+}
+});
+var session=require('express-session');
 // all environments
-app.set('port', process.env.PORT || 3001);
-app.set('views', path.join(__dirname, 'views'));
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(session({
+	secret: 'keboard cat',
+	resace: false,
+	saveUninitialized: true
+}));
 // development only
-
-/*app.get('/', function(req, res){
-	res.send("<script>alert('Hello, Customers');</script>");
-});
-*/
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
-app.get('/', function(req, res){
-	res.render('index.html');
-});
 
+app.get('/', function(req, res){
+   res.render('index.html');
+});
 app.get('/signup', function(req, res){
-	res.render('signup.html');
+   res.render('signup.html');
 });
 
 app.post('/logincheck', function(req, res){
-	if(req.body.id=="test"){
-		if(req.body.pw=="test"){
-			res.send("<h1>WELCOME</h1>");
-		}
-	}
+   var uid=req.body.id;
+   var upw=req.body.pw;
+   var connection=client.query
+   ('SELECT count(*) cnt FROM user WHERE user_email=? and user_pw=?', [uid, upw], function(err,rows){
+      if(err) console.error('err', err);
+      var cnt = rows[0].cnt;
+      if(cnt===1){
+//    	  req.session.user_id=id;
+    	  res.send(
+    			  '<h1>login success</h1>');
+      }else{
+    	  res.json({result:'fail'});
+    	  res.send('<script> alert("id or password is wrong"); history.back(); </script>');
+      }
+   });
 });
 
+app.get('/', routes.index);
+app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
