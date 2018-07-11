fe94b0b0f8090ff53c4ad1394dc5c67599a660c7
pondsuyeon <pondsuyeon@gmail.com>
2018-07-11 21:52:54 +0900

    T.8T

--- app.js
+++ app.js
@@ -110,7 +110,7 @@ app.post('/write', function(req,res){
 	var connection=client.query('INSERT INTO board (writer, title, content) VALUES (?, ?, ?)',[writer, title, content], function(err, result){
 		if(err) console.error('err', err);
 	});
-	res.redirect('/main1');
+	res.render('/main1');
 });
 app.get('/write', function(req,res){
 	res.render('./board', {
